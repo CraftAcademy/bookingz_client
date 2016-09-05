a9922ca463441063543756358bad6a322a79b114
MikaelFeher <mikael.feher@gmail.com>
2016-09-05 20:45:50 +0200

    Adds protractor and jasmine. Sets up protractor browser to use phantomJS

---
+++ tests/features.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+  capabilities: {
+    'browserName': 'phantomjs',
+    'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+  },
+  baseUrl: 'http://localhost:8100',
+  specs: [
+    'features/**/*.feature.js'
+  ],
+
+  jasmineNodeOpts: {
+    isVerbose: true,
+    print: function() {}
+  },
+
+  onPrepare: function() {
+    var SpecReporter = require('jasmine-spec-reporter');
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+  }
+};
