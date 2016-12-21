2026f5ca441a286852f3fd4340e7b69257c4f2f0
tochman <thomas@craftacademy.se>
2016-12-21 18:21:19 +0100

    removes double APP_VERSION entry in constants.js

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,8 +1,7 @@
 angular.module('bookingz.constants', [])
-  .constant('APP_VERSION', '0.0.5')
+  .constant('APP_VERSION', '0.0.6')
   .constant('API_URL_STAGING', 'https://cal-bookingz.herokuapp.com')
   .constant('API_URL', 'https://komboxrum.herokuapp.com')
   .constant('API_URL_DEVELOP', 'http://localhost:3000')
   .constant('SHOW_RESOURCE_NAME', 'false')
-  .constant('SHOW_DATE', 'false')
-  .constant('APP_VERSION', '0.1.3-alpha');
+  .constant('SHOW_DATE', 'false');
