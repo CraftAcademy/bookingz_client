0dcb96b9a5f15ad21ad801206c62615ddbaf70db
tochman <thomas@craftacademy.se>
2016-12-22 09:44:18 +0100

    WIP adding tests

--- tests/features.conf.js
+++ tests/features.conf.js
@@ -1,8 +1,8 @@
+
 exports.config = {
   capabilities: {
     'browserName': 'phantomjs',
-    'phantomjs.binary.path': require('phantomjs-prebuilt').path//,
-    //'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+    'phantomjs.binary.path': require('phantomjs-prebuilt').path
   },
   baseUrl: 'http://localhost:8100',
   specs: [
@@ -16,6 +16,13 @@ exports.config = {
 
   onPrepare: function() {
     var SpecReporter = require('jasmine-spec-reporter');
+    var HtmlReporter = require('protractor-html-screenshot-reporter');
+
     jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+    //screenshot generator - this is not set up properly yet
+    // https://www.npmjs.com/package/protractor-html-screenshot-reporter
+    jasmine.getEnv().addReporter(new HtmlReporter({
+      baseDirectory: 'screenshots', takeScreenShotsOnlyForFailedSpecs: true
+    }));
   }
 };
